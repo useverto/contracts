3762e3709cbaa503c799cbf86eb04122d3a14845
Marton Lederer <marton@lederer.hu>
2021-11-04 22:41:29 +0100

    feat: append LICENSE at the beggining of contracts

--- build.js
+++ build.js
@@ -12,6 +12,8 @@ const fs = require("fs");
     .filter((el) => !el.isDirectory())
     .map(({ name }) => name);
 
+  console.log("Building contracts...");
+
   // build contracts
   await build({
     entryPoints: contractEntries.map((entry) => `./src/${entry}`),
@@ -20,6 +22,8 @@ const fs = require("fs");
     bundle: true
   });
 
+  console.log("Building tests...");
+
   // build tests
   await build({
     entryPoints: testEntries.map((entry) => `./__tests__/${entry}`),
@@ -27,12 +31,28 @@ const fs = require("fs");
     format: "cjs"
   });
 
+  console.log("Getting license...");
+
+  // read license
+  let license = fs.readFileSync("./LICENSE").toString();
+  const lines = license.split("\n");
+
+  // make license a comment
+  for (let i = 0; i < lines.length; i++) {
+    lines[i] = " * " + lines[i];
+  }
+
+  license = "/*\n * \n" + lines.join("\n") + "\n * \n */\n\n";
+
+  console.log("Appending license and fixing exports...");
+
   for (const entry of contractEntries) {
     const filename = `./dist/${entry.replace(/\.ts$/, ".js")}`;
     let src = fs.readFileSync(filename).toString();
 
     src = src.replace("async function handle", "export async function handle");
     src = src.replace("export {\n  handle\n};\n", "");
+    src = license + src;
     fs.writeFileSync(filename, src);
   }
 })();
