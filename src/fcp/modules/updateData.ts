fe680de8f302227b5771583b95df545469a67e92
Marton Lederer <marton@lederer.hu>
2022-03-20 20:55:17 +0100

    chore: small comment

--- src/fcp/modules/updateData.ts
+++ src/fcp/modules/updateData.ts
@@ -1,5 +1,6 @@
 import { ActionInterface, StateInterface, UpdateDataInterface } from "../faces";
 
+// demo function
 export const UpdateData = (state: StateInterface, action: ActionInterface) => {
   const caller = action.caller;
   const input: UpdateDataInterface = action.input;
